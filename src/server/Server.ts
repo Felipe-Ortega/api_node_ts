6fa1af978c7f0be3625372d18d53ca7624f348be
Felipe-Ortega <felipe.osouza@sptech.school>
2025-12-04 09:16:28 -0300

    feat: add folders struture

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-
+import 'dotenv/config';
+import {router} from './routes';
 
 const server = express();
 
-server.get('/', (req, res) => {
-  return res.send('Deu certo');
-});
+server.use(express.json());
+server.use(router);
 
 
 
